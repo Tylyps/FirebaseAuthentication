7c25e2db788f60e073a454efc5c1a28d767fbe99
Tylyps <j.chmielewski@o2.pl>
2018-11-21 21:54:01 +0100

    Add private routes pages

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Router, Route, Switch, Link } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import EditExpensePage from '../components/EditExpensePage';
 import AddExpansePage from '../components/AddExpansePage';
 import HelpPage from '../components/HelpPage';
 import ErrorPage from '../components/ErrorPage';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute'
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact />
-        <Route path="/dashboard" component={ExpenseDashboardPage} exact />
-        <Route path="/create" component={AddExpansePage} exact />
-        <Route path="/edit/:id" component={EditExpensePage} exact />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact />
+        <PrivateRoute path="/create" component={AddExpansePage} exact />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} exact />
         <Route path="/help" component={HelpPage} exact />
         <Route component={ErrorPage} />
       </Switch>
